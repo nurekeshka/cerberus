88eb8d2b6a0a2875aac8a90545d491c3e0a9ac93
nurekeshka <89794613+nurekeshka@users.noreply.github.com>
2022-08-06 14:11:55 +0600

    feat: add csv parsing command

---
+++ passwords/management/commands/parse_excel.py
@@ -0,0 +1,22 @@
+from django.core.management.base import BaseCommand
+from passwords.models import Password
+import pandas as pd
+
+
+def parse_csv(path: str):
+    df = pd.read_csv(path)
+    
+    for i in df.index:
+        Password.objects.create(
+            name=df.iloc[i, 0],
+            url=df.iloc[i, 1],
+            username=df.iloc[i, 2],
+            password=df.iloc[i, 3],
+        )
+
+
+class Command(BaseCommand):
+    help = 'Это команда запуска парсера для менеджера паролей'
+
+    def handle(self, *args, **kwargs):
+        parse_csv(r'C:\Users\Nurbek\OneDrive\Документы\Backup\opera-password.csv')
